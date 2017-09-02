c50509f9f311cb6abb1b3b1514a142fdfcb1c969
Boris <boris.pozzo@gmail.com>
2017-09-02 16:07:47 +0200

    set up firebase authentication

--- src/App.js
+++ src/App.js
@@ -14,17 +14,26 @@ import firebase from 'firebase';
 
 import Login from './Login';
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  }
+});
+
 export default class App extends Component {
-    componentWillMount() {
-        firebase.initializeApp({
-            apiKey: "AIzaSyBnX5i-_s9Qjmvy8bV1n_Qe-FWtwKV-AFk",
-            authDomain: "crm-app-2b59b.firebaseapp.com",
-            databaseURL: "https://crm-app-2b59b.firebaseio.com",
-            projectId: "crm-app-2b59b",
-            storageBucket: "",
-            messagingSenderId: "29437328704"
-        });
-    }
+  componentWillMount() {
+    firebase.initializeApp({
+      apiKey: "AIzaSyBnX5i-_s9Qjmvy8bV1n_Qe-FWtwKV-AFk",
+      authDomain: "crm-app-2b59b.firebaseapp.com",
+      databaseURL: "https://crm-app-2b59b.firebaseio.com",
+      projectId: "crm-app-2b59b",
+      storageBucket: "",
+      messagingSenderId: "29437328704"
+    });
+  }
   render() {
     return (
       <View style={styles.container}>
@@ -34,21 +43,3 @@ export default class App extends Component {
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
--- src/Login.js
+++ src/Login.js
@@ -6,85 +6,117 @@
 
 import React, { Component } from 'react';
 import {
-  StyleSheet,
-  Text,
-  View
+	StyleSheet,
+	Text,
+	View
 } from 'react-native';
 import { MKTextField, MKColor, MKButton } from 'react-native-material-kit';
+import Loader from "./Loader";
+import firebase from 'firebase';
 
 const LoginButton = MKButton.coloredButton()
-    .withText('LOGIN')
-    .build();
+	.withText('LOGIN')
+	.build();
 
 const styles = StyleSheet.create({
-    form: {
-        paddingBottom: 10,
-        width: 200,
-    },
-    fieldStyles: {
-        height: 40,
-        color: MKColor.Orange,
-        width: 200,
-    },
-    loginButtonArea: {
-        marginTop: 20,
-    },
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#F5FCFF',
-  },
-    welcome: {
-        fontSize: 20,
-        textAlign: 'center',
-        margin: 10,
-  },
+	form: {
+		paddingBottom: 10,
+		width: 200,
+	},
+	fieldStyles: {
+		height: 40,
+		color: MKColor.Orange,
+		width: 200,
+	},
+	loginButtonArea: {
+		marginTop: 20,
+	},
+	container: {
+		flex: 1,
+		justifyContent: 'center',
+		alignItems: 'center',
+		backgroundColor: '#F5FCFF',
+	},
+	errorMessage: {
+		marginTop: 15,
+		fontSize: 15,
+		color: 'red',
+		alignSelf: 'center'
+	},
 });
 
 export default class Login extends Component {
-    state = {
-        email: '',
-        password: '',
-        error: ''
-    };
+	state = {
+		email: '',
+		password: '',
+		loading: false,
+		error: ''
+	};
 
-    onButtonPress = () => {
-        console.log('Clicked button !');
-    }
+	onButtonPress = () => {
+		const { email, password } = this.state;
+		this.setState({ error: '', loading: true })
+
+		firebase.auth().signInWithEmailAndPassword(email, password)
+			.then(this.onAuthSuccess())
+			.catch(() => {
+				firebase.auth().createUserWithEmailAndPassword(email, password)
+					.then(this.onAuthSuccess())
+					.catch(this.onAuthFailed())
+			});
+	}
+
+	onAuthSuccess = () => {
+		this.setState({
+			email: '',
+			password: '',
+			loading: false,
+			error: ''
+		})
+	}
+
+	onAuthFailed = () => {
+		this.setState({
+			error: 'Authentication Failed',
+			loading: false
+		})
+	}
+
+	renderLoader = () => {
+		const { loading } = this.state;
+		return loading ? <Loader size="large" /> : <LoginButton onPress={this.onButtonPress} />;
+	}
 
 
-  render() {
-      const { email, password, error } = this.state;
-      const { form, fieldStyles, loginButtonArea, errorMessage, welcome, container } = styles;
+	render() {
+		const { email, password, error } = this.state;
+		const { form, fieldStyles, loginButtonArea, errorMessage, welcome, container } = styles;
 
-    return (
-      <View style={container}>
-        <Text style={welcome}>
-          Welcome to the crm moda fucka!
-        </Text>
-        <MKTextField
-            text={email}
-            onTextChange={email => this.setState({ email })}
-            textInputStyle={fieldStyles}
-            placeholder={'Email...'}
-            tintColor={MKColor.Teal}
-        />
-        <MKTextField
-            text={password}
-            onTextChange={password => this.setState({ password })}
-            textInputStyle={fieldStyles}
-            placeholder={'password...'}
-            tintColor={MKColor.Teal}
-            password={true}
-        />
-        <Text style={errorMessage}>
-            {error}
-        </Text>
-        <View style={loginButtonArea}>
-            <LoginButton onPress={this.onButtonPress()}/>
-        </View>
-      </View>
-    );
-  }
+		return (
+			<View style={form}>
+				<Text> Login or create an account </Text>
+				<MKTextField
+					text={email}
+					onTextChange={email => this.setState({ email })}
+					textInputStyle={fieldStyles}
+					placeholder={'Email...'}
+					tintColor={MKColor.Teal}
+				/>
+				<MKTextField
+					text={password}
+					onTextChange={password => this.setState({ password })}
+					textInputStyle={fieldStyles}
+					placeholder={'password...'}
+					tintColor={MKColor.Teal}
+					password={true}
+				/>
+				<Text style={errorMessage}>
+					{error}
+				</Text>
+				<View style={loginButtonArea}>
+					{this.renderLoader()}
+				</View>
+			</View >
+		);
+	}
 }
